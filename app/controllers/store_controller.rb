6a7ba0207e9c9095cf76abcd903a9964cfe7ec29
Alex Venditti <alvenditti@valdosta.edu>
2018-02-01 10:34:29 -0500

    Finished part 2

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+	include CurrentCart
+	before_action :set_cart, only: [:index]	
   def index
-  	@products = Product.order(:title)
+  	@products = Product.order(:popularity)
+	if session[:counter].nil?
+		session[:counter] = 0;
+	else
+		session[:counter] += 1
+		@counter = session[:counter]
+	end
   end
 end
