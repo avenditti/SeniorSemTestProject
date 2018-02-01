617c5662bc8cdef825a8d6df00c7d4a00ce0792f
Alex Venditti <alvenditti@valdosta.edu>
2018-02-01 16:58:45 -0500

    Reversed Sort order on main page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,7 @@ class StoreController < ApplicationController
 	include CurrentCart
 	before_action :set_cart, only: [:index]	
   def index
-  	@products = Product.order(:popularity)
+  	@products = Product.order(:popularity).reverse_order
 	if session[:counter].nil?
 		session[:counter] = 0;
 	else
