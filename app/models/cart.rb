0372e91d408e1b05613293b70a07e042e847a90c
Alex Venditti <alvenditti@valdosta.edu>
2018-02-08 08:58:54 -0500

    Troubleshooting bugfix

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,8 +3,10 @@ class Cart < ApplicationRecord
 
 	def add_product(product)
 		current_item = line_items.find_by(product_id: product.id)
+		print "HERE"
 		if current_item
 			current_item.quantity += 1
+			print "INCREASED!"
 		else
 			current_item = line_items.build(product_id: product.id)
 		end
